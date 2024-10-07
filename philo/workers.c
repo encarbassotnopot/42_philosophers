14b8d18afe65d9bc8c137640020cfed704f2d3d3
Eina Safor Coma Bages <ecoma-ba@student.42barcelona.com>
2024-10-07 16:02:00 +0200

    arreglada cosa de mutex que he cagat

--- philo/workers.c
+++ philo/workers.c
@@ -6,7 +6,7 @@
 /*   By: ecoma-ba <ecoma-ba@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 17:16:05 by ecoma-ba          #+#    #+#             */
-/*   Updated: 2024/10/07 13:30:42 by ecoma-ba         ###   ########.fr       */
+/*   Updated: 2024/10/07 15:53:28 by ecoma-ba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ void	necromancer(t_winfo *w)
 			pthread_mutex_lock(&w->ph_muts[i]);
 			if (w->phinfos[i].ate < w->params[MEALS] && w->params[MEALS])
 				ate_enough = 0;
-			if (time_diff(&w->phinfos[i].last_meal, &tv) > w->params[TTDIE] * 1000)
+			if (time_diff(&w->phinfos[i].last_meal, &tv) / 1000 > w->params[TTDIE])
 			{
 				print_msg(&w->phinfos[i], "died");
 				body_count++;
